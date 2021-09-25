fe1f8b64481299a82c74a0b2ea866f57eebfc8d6
u7224266 <u7224266@anu.edu.au>
2021-09-25 14:27:03 +0800

    u7224266

---
+++ app/src/main/java/com/example/myapplication/user.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class user extends AppCompatActivity {
+
+    private Button login;
+    private Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+
+    }
+}
