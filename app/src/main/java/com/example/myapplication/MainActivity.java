5e089c16633de415913a4bbee995d38fb2fe6253
Jiaan Guo <u7323660@anu.edu.au>
2021-09-27 15:50:03 +0800

    integrate register, login, logout feature with firebase

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,54 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private FirebaseAuth mAuth;
+    private TextView logoutButton;
+    private TextView usernameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logoutButton = findViewById(R.id.logout_button);
+        usernameTextView = findViewById(R.id.username);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() == null) {
+            startActivity(new Intent(MainActivity.this, StartActivity.class));
+        } else {
+            if(mAuth.getCurrentUser()!= null) {
+                usernameTextView.setText(mAuth.getCurrentUser().getEmail());
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.logout_button) {
+            mAuth = FirebaseAuth.getInstance();
+            mAuth.signOut();
+            startActivity(new Intent(MainActivity.this, StartActivity.class)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+            finish();
+            Toast.makeText(MainActivity.this, "Logout succeed", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/MessageActivity.java
+++ app/src/main/java/com/example/myapplication/MessageActivity.java
@@ -3,18 +3,12 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class user extends AppCompatActivity {
-
-    private Button login;
-    private Button register;
+public class MessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
-
-
+        setContentView(R.layout.activity_message);
     }
 }
