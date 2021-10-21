122ef01ef886c7a0f770e60c9b3b0923b5efa7a9
Jiaan Guo <u7323660@anu.edu.au>
2021-10-22 07:44:56 +0800

    add paint functionaility

--- app/src/main/java/com/example/myapplication/chat/FriendAdapter.java
+++ app/src/main/java/com/example/myapplication/chat/FriendAdapter.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myapplication.login.User;
+
 import java.util.List;
 
 public class friendAdapter extends RecyclerView.Adapter<friendAdapter.friendHolder> {
